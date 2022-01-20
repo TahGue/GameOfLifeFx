5ddf3825587ca7b77ecac7b831f672d4754733c7
tahar <taharguemir@gmail.com>
2022-01-20 20:59:02 +0100

    Second commit

--- src/main/java/com/example/gameoflifefx/Cell.java
+++ src/main/java/com/example/gameoflifefx/Cell.java
@@ -1,11 +1,26 @@
 package com.example.gameoflifefx;
 
 public class Cell {
-    public CellState  state;
-    public Cell (CellState state){
+
+    private CellState state;
+
+    public Cell(CellState state) {
         this.state = state;
     }
-    public enum CellState{
-        Alive, DEAD
+
+    public enum CellState {
+        ALIVE, DEAD
+    }
+
+    public void update(int i) {
+        if (state == CellState.ALIVE) {
+            state = i > 1 && i < 4 ? CellState.ALIVE : CellState.DEAD;
+        } else {
+            state = i == 3 ? CellState.ALIVE : CellState.DEAD;
+        }
+    }
+
+    public CellState getState() {
+        return state;
     }
 }
--- src/test/java/CellTest.java
+++
@@ -1,9 +0,0 @@
-import com.example.gameoflifefx.Cell;
-import org.junit.jupiter.api.Test;
-
-public class CellTest {
-    @Test
-    public void ShouldDieWithOnlyOneNeighbor(){
-       Cell wwt = new Cell(Cell.CellState.Alive);
-    }
-}
